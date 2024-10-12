3086c5a4dc81de3df0f4662ddc2f69cc13a793d0
serendipity <976758426@qq.com>
2024-10-12 18:44:02 +0800

    CMakeLearn-set

---
+++ code/v2/build/CMakeFiles/app2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/add.cpp.o"
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/add.cpp.o.d"
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/div.cpp.o"
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/div.cpp.o.d"
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/main.cpp.o"
+  "CMakeFiles/app2.dir/home/serenNan/CMakeLearn/code/v1/main.cpp.o.d"
+  "app2"
+  "app2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ code/v2/build/CMakeFiles/app2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for app2.
+# This may be replaced when dependencies are built.
